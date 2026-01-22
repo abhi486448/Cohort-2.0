cf186e47ad32fd5fb889161e9113455bff69845d
abhi486448 <ag096129abhishek@gmail.com>
2026-01-22 18:51:29 +0530

    Add L27

---
+++ Front-End/27-JavaScript_Basics/script.js
@@ -0,0 +1,26 @@
+// console... prompt alert
+  //console.log("neyh");
+  //console.info("neyh");
+  //console.warn("neyh");
+  //console.error("neyh");
+  //console.table({ name: "harsh" });
+
+  //let nm = prompt("name ?"); // help to ask anything
+  //console.log(nm);
+
+  //alert("hay"); // it tell something
+
+// slice template-string (template literal ``) split replace includes
+  //console.log("harsh bhai kaise ho".slice(6,10)); // bhai b=6 i=9
+  //console.log("harsh 4+4");// this will not calculate 4+4
+  //console.log(`harsh ${4+4}`); // but now we can calculate 4+4
+
+// split replace include
+  //console.log("harsh".split());
+  //console.log("harsh".split(''));
+  //console.log("harsh and rahul".split(' '));
+
+  //console.log("harsh bhai kaise ho".replace('h','g')); // only first 'h' will replased
+  //console.log("harsh bhai kaise ho".replaceAll('h','g')); // this will replace all 'h' with 'g'
+
+  //console.log("harsh bhai kaise ho".includes('h')); // this will check wether h is there in the string or not
