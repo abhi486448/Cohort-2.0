05c813f453586956d71cb233f39895dd1a075565
abhi486448 <ag096129abhishek@gmail.com>
2026-02-04 03:27:37 +0530

    Add L69

---
+++ Front-End/69-React_Basics/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './component/Nav'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      this is App
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ Front-End/69-React_Basics/src/component/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        this is Nav
+    </div>
+  )
+}
+
+export default Nav
