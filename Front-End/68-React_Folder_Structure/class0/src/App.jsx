92f1ee99acc7023754570ea574924e9b3b3171a0
abhi486448 <ag096129abhishek@gmail.com>
2026-02-04 01:22:36 +0530

    Add L68

---
+++ Front-End/68-React_Folder_Structure/class0/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello guys, from App</h1>
+      <h2>Cohort 2.0 is good course</h2>
+    </div>
+  )
+}
+
+export default App
