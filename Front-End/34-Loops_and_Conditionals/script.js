734893c10bda522bf888bc1902fa8fa3d5d0171c
abhi486448 <ag096129abhishek@gmail.com>
2026-02-05 17:48:41 +0530

    Add L34

---
+++ Front-End/34-Loops_and_Conditionals/script.js
@@ -0,0 +1,6 @@
+// Q.12. Ask user for words until they type “stop”. Count how many times they typed “yes”
+let pass = prompt("tum mujhe password do");
+while(pass !== "stop"){
+    pass = prompt("tum mujhe password do");
+    console.log(pass);
+}
