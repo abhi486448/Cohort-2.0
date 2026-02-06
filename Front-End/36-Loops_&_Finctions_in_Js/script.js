698b9dfadba066a5bb79abe56aacd358a6d1b6ca
abhi486448 <ag096129abhishek@gmail.com>
2026-02-06 18:44:43 +0530

    Add L36

---
+++ Front-End/36-Loops_&_Finctions_in_Js/script.js
@@ -0,0 +1,22 @@
+// Ewcursion in JavaScript
+// let i=0;
+// function Add(){
+//     console.log(i);
+//     i++;
+//     if(i<101) Add(); // Finction calling itself is recursion
+// }
+// Add();
+
+// continue
+// for(let i = 1; i < 10; i++){
+//     if(i === 5) continue; // 5 will be skiped
+//     console.log(i);
+// }
+
+// functions
+
+    //argument key
+    function doSomething(){
+        console.log(arguments); // argument keyward use to axcess all arguments given to a function
+    }
+    doSomething("Abhishek", 23, "test@test.com");
