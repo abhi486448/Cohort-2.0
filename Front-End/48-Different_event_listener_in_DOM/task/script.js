dca0dbf01d073b3c4c0a252367743f2147a616c8
abhi486448 <ag096129abhishek@gmail.com>
2026-04-10 16:19:09 +0530

    Add L48 Task

--- Front-End/48-Different_event_listener_in_DOM/task/script.js
+++ Front-End/48-Different_event_listener_in_DOM/task/script.js
@@ -1,32 +1,27 @@
 var piano = document.querySelector(".part-2");
+var audio = new Audio;
+var idx = 21;
+var arr = ['C','D','E','F','G','A','B'];
+var arr1 = ['Q','W','E','R','T','Y','U','I','O','P','Z','X','C','V','B','N','M',',','.','/'];
+var arr3=['2','3','5','6','7','9','0','S','D','F','H','J','L',';',`'`];
 
 function appendBtn(){
     var clutter = "";
-    var arr = ['C','D','E','F','G','A','B'];
-    var arr1 = ['Q','W','E','R','T','Y','U','I','O','P','Z','X','C','V','B','N','M',',','.','/'];
-    var SBTN = [
-        {   
-            1:{left : 22},
-            2:{left : 44},
-        },
-        {
-            1:{left : 66},
-            2:{left : 88},
-            3:{left : 110},
-        }
-    ]
+    
     var count = 0;
     var j=0;
     var clutter1 = "";
-    for(i=1; i<=36; i++){
+    for(let i=1; i<=36; i++){
         if(i>=8 && j<arr1.length){
-            clutter1 = `<div class="extra">${arr1[j]}</div>`;
+            clutter1 = `<div class="extra" data-index="${idx}" data-name="${arr1[j]}">${arr1[j]}</div>`;
             j++;
+            idx++;
         } else {
-            clutter1 = "";
+            clutter1 = `<div class="extra" data-index="${idx}"></div>`;
+            idx++;
         }
         var car = arr[count];
-        clutter += `<div class="btn">
+        clutter += `<div class="btn btn-${i}" >
                 <div>
                     ${clutter1}
                     <h4>${car}</h4>
@@ -39,14 +34,73 @@ function appendBtn(){
         }
     }
 
-    for(i=1; i<=25; i++){
-        clutter += `<div class="s-btn-${i}"></div>`;
-        document.querySelector("`.s-btn-${i}`").style.left = (i*22) +"px";
-        document.querySelector("`.s-btn-${i}`").style.top = 0+"px";
+    var clutter3 = "";
+    var L = 22;
+    var LQ = [40,64,40,40,64]
+    var count = 0;
+    var count1 = 0;
+    var clutter4 = "";
+    for(let i=1; i<=25; i++){
+        var elem = document.createElement("div");
+        if(i>=6 && count1<=arr3.length-1){
+            clutter4 = `<div class="extra extra1" data-index="${idx}" data-name="${arr3[count1]}">${arr3[count1]}</div>`;
+            elem.innerHTML = clutter4;
+            count1++;
+            idx++;
+            elem.classList = `s-btn-${i}, s-btn`;
+        } else {
+            elem.classList = `s-btn-${i}, s-btn`;
+            elem.setAttribute("data-index",`${idx}`);
+            idx++;
+        }
+        elem.style.left = (L) +"px";
+        elem.style.top = "0px";
+        clutter3 += elem.outerHTML;
+        if(count>4){
+            count = 0;
+            L += LQ[count];
+        } else L += LQ[count];
+
+        count++;
+
     }
 
 
     piano.innerHTML = clutter;
+    piano.insertAdjacentHTML("beforeend", clutter3);
 }
 
+function OnClickScreen(){
+    piano.addEventListener("click", function(elm){
+        FindKeyAndPlay(elm.target.attributes[1].value);
+    })
+}
+
+function OnPressKey(){
+    
+    document.body.addEventListener("keydown", function(elm){
+        var n = 0;
+
+        if(arr1.includes(elm.key.toUpperCase())){
+            n = arr1.indexOf(elm.key.toUpperCase());
+            FindKeyAndPlay(n+27);
+            // var elem = document.querySelector(`.btn-${n+7}`);
+            // elem.style.backgroundColor = "radial-gradient(circle,rgba(255, 255, 255, 1) 46%, rgba(148, 187, 233, 1) 100%)"
+            // console.log(elem);
+        }
+        if(arr3.includes(elm.key.toUpperCase())){
+            n = arr3.indexOf(elm.key.toUpperCase()) + 61;
+            FindKeyAndPlay(n);
+        }
+    })
+}
+
+function FindKeyAndPlay(elm){
+    console.log(elm);
+    audio.src = `./songs/${elm}.mp3`;
+    audio.play();
+}
+
+OnPressKey();
+OnClickScreen()
 appendBtn();
