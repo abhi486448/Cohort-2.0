0d3a277f6d79ef13b58401721060034eaa698792
abhi486448 <ag096129abhishek@gmail.com>
2026-06-01 16:15:00 +0530

    Add .env in BL6

--- Backend/6-Database/server.js
+++ Backend/6-Database/server.js
@@ -2,11 +2,12 @@
  * - server ko start krna
  * - database se connect krna
  */
+require("dotenv").config()
 const app = require("./src/app")
 const mongoose = require("mongoose")
 
 function connectToDb() {
-    mongoose.connect("mongodb://Abhishek:HShsHjguGo374ayW@ac-le5bl64-shard-00-00.eoa4ojn.mongodb.net:27017,ac-le5bl64-shard-00-01.eoa4ojn.mongodb.net:27017,ac-le5bl64-shard-00-02.eoa4ojn.mongodb.net:27017/?ssl=true&replicaSet=atlas-s029zv-shard-0&authSource=admin&appName=Cluster0/day-6")
+    mongoose.connect(process.env.MONGO_URI)
         .then(() => {
             console.log("Connected To Database")
         })
