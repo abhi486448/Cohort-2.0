a376e47cc72d4ea09a72ebd0fa881f61ea3c7210
abhi486448 <ag096129abhishek@gmail.com>
2026-06-01 14:00:09 +0530

    update database

--- Backend/6-Database/server.js
+++ Backend/6-Database/server.js
@@ -6,14 +6,16 @@ const app = require("./src/app")
 const mongoose = require("mongoose")
 
 function connectToDb() {
-    mongoose.connect("mongodb://localhost:27017/day-6")
+    mongoose.connect("mongodb://Abhishek:HShsHjguGo374ayW@ac-le5bl64-shard-00-00.eoa4ojn.mongodb.net:27017,ac-le5bl64-shard-00-01.eoa4ojn.mongodb.net:27017,ac-le5bl64-shard-00-02.eoa4ojn.mongodb.net:27017/?ssl=true&replicaSet=atlas-s029zv-shard-0&authSource=admin&appName=Cluster0/day-6")
         .then(() => {
             console.log("Connected To Database")
         })
-}
 
+}
 connectToDb()
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
